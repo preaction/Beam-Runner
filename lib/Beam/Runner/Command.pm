d9d054fa942547575e1f2ea5228fd03002db9c55
Doug Bell <doug@preaction.me>
2016-12-27 00:04:44 -0600

    add basic run command

--- lib/Beam/Runner/Command.pm
+++ lib/Beam/Runner/Command.pm
@@ -3,14 +3,29 @@ package Beam::Runner::Command;
 
 =head1 SYNOPSIS
 
+    exit Beam::Runner::Command->run( $cmd => @args );
+
 =head1 DESCRIPTION
 
+This is the entry point for the L<beam> command which loads and
+runs the specific C<Beam::Runner::Command> class.
+
 =head1 SEE ALSO
+
+The L<Beam::Runner> commands: L<Beam::Runner::Command::run>,
+L<Beam::Runner::Command::list>, L<Beam::Runner::Command::help>
 
 =cut
 
 use strict;
 use warnings;
+use Module::Runtime qw( use_module compose_module_name );
+
+sub run {
+    my ( $class, $cmd, @args ) = @_;
+    my $cmd_class = compose_module_name( 'Beam::Runner::Command', $cmd );
+    return use_module( $cmd_class )->run( @args );
+}
 
 1;
 
--- lib/Beam/Runner/Command/run.pm
+++ lib/Beam/Runner/Command/run.pm
@@ -17,9 +17,40 @@ L<beam>, L<Beam::Runner::Command>, L<Beam::Runner>
 
 use strict;
 use warnings;
+use Beam::Wire;
+use Path::Tiny qw( path );
+
+# File extensions to try to find, starting with no extension (which is
+# to say the extension is given by the user's input)
+my @EXTS = ( "", qw( .yml .yaml .json .xml .pl ) );
 
 sub run {
+    my ( $class, $container, $service_name, @args ) = @_;
+    my $path;
+    if ( path( $container )->is_file ) {
+        $path = path( $container );
+    }
+    else {
+        DIR: for my $dir ( ".", split /:/, $ENV{BEAM_PATH} ) {
+            my $d = path( $dir );
+            for my $ext ( @EXTS ) {
+                my $f = $d->child( $container . $ext );
+                if ( $f->exists ) {
+                    $path = $f;
+                    last DIR;
+                }
+            }
+        }
+    }
 
+    die qq{Could not find container $container in directories .:$ENV{BEAM_WIRE}}
+        unless $path;
+
+    my $wire = Beam::Wire->new(
+        file => $path,
+    );
+    my $service = $wire->get( $service_name );
+    return $service->run( @args );
 }
 
 1;
