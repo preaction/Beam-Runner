5e6478fa4070d036eb7d04dafa4fe5dd5843f4d3
Doug Bell <doug@preaction.me>
2017-05-08 14:09:10 -0500

    clarify the project's use-case and goals
    Fixes #2

--- lib/Beam/Runner.pm
+++ lib/Beam/Runner.pm
@@ -1,6 +1,6 @@
 package Beam::Runner;
 our $VERSION = '0.008';
-# ABSTRACT: Execute runnable objects from Beam::Wire containers
+# ABSTRACT: Configure, list, document, and execute runnable objects
 
 =head1 SYNOPSIS
 
@@ -12,17 +12,17 @@ our $VERSION = '0.008';
 
 =head1 DESCRIPTION
 
-This distribution is an execution and organization system for
-L<Beam::Wire> containers. This allows you to prepare executable objects
-in configuration files and then execute them. This also allows easy
-discovery of container files and objects, and allows you to document
+This distribution is an execution and organization system for runnable
+objects. This allows you to prepare a list of runnable tasks in
+configuration files and then execute them. This also allows easy
+discovery of configuration files and objects, and allows you to document
 your objects for your users.
 
 =head2 Container Files
 
-The container file is a configuration file that describes services. Some
-of these services are marked as executable by using the L<Beam::Runnable>
-role.
+The configuration file is a L<Beam::Wire> container file that describes
+services. Some of these services are marked as executable by consuming the
+L<Beam::Runnable> role.
 
 The container file can have a special service called C<$summary> which
 has a short summary that will be displayed when using the C<beam list>
@@ -49,13 +49,13 @@ C<CPAN::Testers::Backend::Migrate::ToMetabase>:
 For more information about container files, see L<the Beam::Wire
 documentation|Beam::Wire>.
 
-=head2 Services
+=head2 Tasks
 
-A service is an object configured in the container file. C<Beam::Runner>
+A task is an object configured in the container file. C<Beam::Runner>
 works with executable objects that consume the L<Beam::Runnable> role.
 This role requires only a C<run()> method be implemented in the class.
 
-Services are expected to have documentation that will be displayed by
+Tasks are expected to have documentation that will be displayed by
 the C<beam list> and C<beam help> commands. The C<beam list> command
 will display the C<NAME> section of the documentation, and the C<beam
 help> command will display the C<NAME>, C<SYNOPSIS>, C<DESCRIPTION>,
