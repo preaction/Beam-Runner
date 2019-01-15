a886fdc3561f8458db61c8384a7bfe34a5123c60
Doug Bell <doug@preaction.me>
2019-01-14 23:05:06 -0500

    add steps runnable command
    This command runs a series of other commands.
    
    This is mostly going to be used in the future by Beam::Minion to create
    multi-step jobs in Minion.

---
+++ lib/Beam/Runner/Steps.pm
@@ -0,0 +1,46 @@
+package Beam::Runner::Steps;
+our $VERSION = '0.015';
+# ABSTRACT: Run a series of steps
+
+=head1 SYNOPSIS
+
+    beam run <container> <service>
+
+=head1 DESCRIPTION
+
+This runnable module runs a series of other runnable modules in
+sequence. If any module returns a non-zero value, the steps stop.
+
+=head1 SEE ALSO
+
+L<beam>, L<Beam::Runnable>
+
+=cut
+
+use Moo;
+use warnings;
+with 'Beam::Runnable';
+use Types::Standard qw( ArrayRef ConsumerOf );
+
+=attr steps
+
+The steps to run. Must be an arrayref of L<Beam::Runnable> objects.
+
+=cut
+
+has steps => (
+    is => 'ro',
+    isa => ArrayRef[ConsumerOf['Beam::Runnable']],
+    required => 1,
+);
+
+sub run {
+    my ( $self, @args ) = @_;
+    for my $step ( @{ $self->steps } ) {
+        my $exit = $step->run( @args );
+        return $exit if $exit != 0;
+    }
+    return 0;
+}
+
+1;
